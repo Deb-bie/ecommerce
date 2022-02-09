a8a8a51f32663ed99c152c9f7d010141f4c83ade
Debbie <asamoahjaeldeborah@gmail.com>
2022-02-09 02:14:35 +0000

    Designed the navbar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,315 @@
+import React , { useState } from 'react';
+import { MainNav, NavContainer, NavLogo, Name, Profile, ProfileIcon, BrandName, Icon, PContent, MenuBar,
+    PIcon, Dropdown, PCLink, NavMenu, NavItem, NavLink, Y, NavDropdown, NavContent, NCLink, NIcon, NavContent2,
+    NavContent3, NavContent4, NavContent5, Z, Z1, Z2, Z3, Z4, AnnouncementContainer, ItemsNav, ItemsContainer, Items, 
+    Item, IDropdown,ItemIcon, ItemContent, ItemLink, ItemsList } from './navbarStyles';
+import { Badge } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo/logo_8.svg';
+import {PersonOutlined, ShoppingCartOutlined, FavoriteBorderOutlined, MenuOutlined, CloseOutlined, KeyboardArrowDownOutlined} from '@material-ui/icons';
+import { auth} from '../../firebase/firebase'
+
+
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => {
+        setClick(!click);
+    }
+
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setClick(!click);
+    }
+
+
+    
+
+
+
+
+    return (
+        <>
+
+            {click ? (
+                <AnnouncementContainer></AnnouncementContainer>
+            ) : (
+                <AnnouncementContainer display>
+                    Super Deal! Free Shipping on Orders Over <span>&nbsp; GH&#8373;</span>50
+                </AnnouncementContainer>
+            )}
+
+            
+
+
+            <MainNav>
+                <NavContainer>
+                    <NavLogo>
+                        <Link to='/'>
+                            <img src={Logo} alt='logo' />
+                        </Link>
+                    </NavLogo>
+
+                    <BrandName>
+                        <Name to='/'>DEE.</Name>
+                    </BrandName>
+                    
+
+                    <Profile>
+                        {auth.currentUser ? (
+                            <>
+                                <ProfileIcon>
+                                    <Icon to='#'>{auth.currentUser.displayName}</Icon>
+                                </ProfileIcon>
+
+                                <ProfileIcon>
+                                    <Badge badgeContent={4} color="primary">
+                                        <Icon to='/cart'><ShoppingCartOutlined /></Icon>
+                                    </Badge>
+                                </ProfileIcon>
+
+
+                                <ProfileIcon>
+                                    <Badge badgeContent={1} color="primary">
+                                        <Icon to='/favorite'><FavoriteBorderOutlined /></Icon>
+                                    </Badge>
+                                </ProfileIcon>
+                            </>
+                        ) : (
+                            <>
+                                <ProfileIcon>
+                                    <Dropdown>
+                                        <PIcon to='#'><PersonOutlined /></PIcon>
+
+                                        <PContent>
+                                            <PCLink to='#'>Login</PCLink>
+                                            <PCLink to='#'>Sign Up</PCLink>
+                                        </PContent>
+                                    </Dropdown>
+                                </ProfileIcon>
+
+                                <ProfileIcon>
+                                    <Badge badgeContent={1} color="primary">
+                                        <Icon to='/cart'><ShoppingCartOutlined /></Icon>
+                                    </Badge>
+                                </ProfileIcon>
+
+
+                                <ProfileIcon>
+                                    <Badge badgeContent={1} color="primary">
+                                        <Icon to='/favorite'><FavoriteBorderOutlined /></Icon>
+                                    </Badge>
+                                </ProfileIcon>
+                            </>
+                        )}
+                        
+                    </Profile>
+
+                    <MenuBar onClick={handleClick}>
+                        {click ? (
+                            <Y><CloseOutlined style={{fontSize: "40px"}} /></Y>
+                        ) : (
+                            <MenuOutlined style={{fontSize: "40px"}}/>
+                        )}
+                    </MenuBar>
+
+
+
+                    {click ? (
+                        <NavMenu>
+                            <NavItem>
+                                <NavLink to='#'>Home</NavLink>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavDropdown>
+                                    <NIcon to='#'>
+                                        Tops
+                                        <Z><KeyboardArrowDownOutlined style={{fontSize: '40px'}}/></Z>
+                                    </NIcon>
+
+                                    <NavContent>
+                                        <NCLink to='#'>T-Shirt</NCLink>
+                                        <NCLink to='#'>Blouses</NCLink>
+                                        <NCLink to='#'>Tank Tops</NCLink>
+                                    </NavContent>
+                                </NavDropdown>
+                            </NavItem>         
+
+                            <NavItem>
+                                <NavDropdown>
+                                    <NIcon to='#'>
+                                        Dresses
+                                        <Z1><KeyboardArrowDownOutlined style={{fontSize: '40px'}}/></Z1>
+                                    </NIcon>
+
+                                    <NavContent2>
+                                        <NCLink to='#'>T-Shirt</NCLink>
+                                        <NCLink to='#'>Blouses</NCLink>
+                                        <NCLink to='#'>Tank Tops</NCLink>
+                                    </NavContent2>
+                                </NavDropdown>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavDropdown>
+                                    <NIcon to='#'>
+                                        Shoes
+                                        <Z2><KeyboardArrowDownOutlined style={{fontSize: '40px'}}/></Z2>
+                                    </NIcon>
+
+                                    <NavContent3>
+                                        <NCLink to='#'>T-Shirt</NCLink>
+                                        <NCLink to='#'>Blouses</NCLink>
+                                        <NCLink to='#'>Tank Tops</NCLink>
+                                    </NavContent3>
+                                </NavDropdown>
+                            </NavItem>     
+
+                            <NavItem>
+                                <NavDropdown>
+                                    <NIcon to='#'>
+                                        Bags
+                                        <Z3><KeyboardArrowDownOutlined style={{fontSize: '40px'}}/></Z3>
+                                    </NIcon>
+
+                                    <NavContent4>
+                                        <NCLink to='#'>T-Shirt</NCLink>
+                                        <NCLink to='#'>Blouses</NCLink>
+                                        <NCLink to='#'>Tank Tops</NCLink>
+                                    </NavContent4>
+                                </NavDropdown>
+                            </NavItem>     
+
+                            <NavItem>
+                                <NavDropdown>
+                                    <NIcon to='#'>
+                                        Accessories
+                                        <Z4><KeyboardArrowDownOutlined style={{fontSize: '40px'}}/></Z4>
+                                    </NIcon>
+
+                                    <NavContent5>
+                                        <NCLink to='#'>T-Shirt</NCLink>
+                                        <NCLink to='#'>Blouses</NCLink>
+                                        <NCLink to='#'>Tank Tops</NCLink>
+                                    </NavContent5>
+                                </NavDropdown>
+                            </NavItem>            
+                        </NavMenu>
+                    ) : (
+                        <></>
+                    )}
+
+                </NavContainer>
+
+            </MainNav>
+
+
+
+            {/* Items */}
+
+            <ItemsNav>
+                <ItemsContainer>
+                    <Items>
+                        <ItemsList>
+                            <Item to='#'>Home</Item>
+                        </ItemsList>
+
+                        <ItemsList>
+                            <IDropdown>
+                                <ItemIcon to="#">
+                                    Tops
+                                    <KeyboardArrowDownOutlined />
+                                </ItemIcon>
+
+                                <ItemContent>
+                                    <ItemLink to='#'>T-Shirt</ItemLink>
+                                    <ItemLink to='#'>Blouses</ItemLink>
+                                    <ItemLink to='#'>Tank Tops</ItemLink>
+                                </ItemContent>
+
+                            </IDropdown>
+                        </ItemsList>
+
+
+                        <ItemsList>
+                            <IDropdown>
+                                <ItemIcon to="#">
+                                    Dresses
+                                    <KeyboardArrowDownOutlined />
+                                </ItemIcon>
+
+                                <ItemContent>
+                                    <ItemLink to='#'>Short Dresses</ItemLink>
+                                    <ItemLink to='#'>Midi Dresses</ItemLink>
+                                    <ItemLink to='#'>Cami Dresses</ItemLink>
+                                </ItemContent>
+
+                            </IDropdown>
+                        </ItemsList>
+
+
+                        <ItemsList>
+                            <IDropdown>
+                                <ItemIcon to="#">
+                                    Shoes
+                                    <KeyboardArrowDownOutlined />
+                                </ItemIcon>
+
+                                <ItemContent>
+                                    <ItemLink to='#'>Heels</ItemLink>
+                                    <ItemLink to='#'>Sneakers</ItemLink>
+                                    <ItemLink to='#'>Sandals</ItemLink>
+                                </ItemContent>
+
+                            </IDropdown>
+                        </ItemsList>
+
+
+                        <ItemsList>
+                            <IDropdown>
+                                <ItemIcon to="#">
+                                    Bags
+                                    <KeyboardArrowDownOutlined />
+                                </ItemIcon>
+
+                                <ItemContent>
+                                    <ItemLink to='#'>Purses</ItemLink>
+                                    <ItemLink to='#'>Handbags</ItemLink>
+                                    <ItemLink to='#'>School bags</ItemLink>
+                                </ItemContent>
+
+                            </IDropdown>
+                        </ItemsList>
+
+
+                        <ItemsList>
+                            <IDropdown>
+                                <ItemIcon to="#">
+                                    Accessories
+                                    <KeyboardArrowDownOutlined />
+                                </ItemIcon>
+
+                                <ItemContent>
+                                    <ItemLink to='#'>Belts</ItemLink>
+                                    <ItemLink to='#'>Hats</ItemLink>
+                                    <ItemLink to='#'>Necklaces</ItemLink>
+                                </ItemContent>
+
+                            </IDropdown>
+                        </ItemsList>
+                    </Items>
+                </ItemsContainer>
+            </ItemsNav>
+        </>
+    )
+}
+
+
+export default Navbar;
+
+
---
+++ src/components/navbar/navbarStyles.js
@@ -0,0 +1,504 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+
+export const AnnouncementContainer = styled.div`
+    height: 30px; 
+    background-color: teal;
+    color: white;
+    display: ${({display}) => (display ? 'flex': "none" )};
+    align-items: center;  
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+    transition: 0.5s ease-in-out ;
+    position: sticky;
+`;
+
+
+export const MainNav = styled.nav`
+    height: 70px;
+    max-width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+
+`;
+
+export const NavContainer = styled.div`
+    display: flex;
+    height: 70px;
+    align-content: center;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`;
+
+
+export const NavLogo = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    margin-left: 30px;
+
+
+    @media (max-width: 648px) {
+        margin-left: 10px;
+    }
+`;
+
+
+export const BrandName = styled.h1`
+    flex: 1 ;
+    text-align: center;
+    text-decoration: none;
+
+`;
+
+
+export const Name = styled(Link)`
+    text-decoration: none;
+    color: #0b4a54;
+    font-size: 40px;
+
+
+    @media (max-width: 648px) {
+        font-size: 30px;
+    }
+`;
+
+
+export const Profile = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-right: 30px;
+
+
+    @media (max-width: 648px) {
+        display: none;
+    }
+
+`;
+
+
+export const ProfileIcon = styled.div`
+    margin-right: 20px;
+`;
+
+
+export const Icon = styled(Link)`
+    color: #000;
+`;
+
+export const Dropdown = styled.div`
+    float: left;
+    overflow: hidden;
+`;
+
+
+export const PIcon = styled(Link)`
+    color: #000;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    text-decoration: none;
+    background-color: inherit;
+    font-family: inherit;
+`;
+
+
+export const PContent = styled.div`
+    display: none;
+    position: absolute;
+    background-color: #0b4a54;
+    min-width: 160px;
+    top: 80px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    color: #fff;
+
+
+    ${Dropdown}:hover & {
+        display: block;
+    }
+
+`;
+
+
+export const PCLink = styled(Link)`
+    float: none;
+    color: #fff;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+
+    &:hover{
+        background-color: #ddd;
+        color: #000;
+    }
+`;
+
+
+export const MenuBar = styled.div`
+    display: none;
+
+
+    @media (max-width: 648px) {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        margin-right: 30px;
+    }
+
+`;
+
+export const Y = styled.div`
+    z-index: 4;
+    color: #fff;
+`;
+
+
+export const NavMenu = styled.ul`
+    display: none;
+
+    @media (max-width: 648px) {
+        display: flex;
+        flex-direction: column;
+        background-color: #0b4a54;
+        width: 100%;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        padding-top: 10px;
+        color: #fff;
+        padding-top: 50px;
+        z-index: 3;
+
+        
+    }    
+`;
+
+
+export const NavItem = styled.li`
+    @media (max-width: 648px) {
+        display: flex;
+    }
+`;
+
+export const NavLink = styled(Link)`
+    @media (max-width: 648px) {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        width: 100%;
+        border-top: 2px solid #000;
+        border-bottom: 2px solid #000;
+        padding-left: 30px;
+        color: #fff;
+        font-size: 24px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+`;
+
+
+export const NavDropdown = styled.div`
+    float: none;
+    overflow: hidden;
+    width: 100%;
+`;
+
+
+export const NIcon = styled(Link)`
+    width: 100%;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    text-decoration: none;
+    background-color: inherit;
+    font-family: inherit;
+    font-size: 24px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #000;
+    padding-left: 30px;
+`;
+
+
+export const Z = styled.div`
+    padding-left: 70%;
+`;
+
+
+export const Z1 = styled.div`
+    padding-left: 64%;
+`;
+
+
+export const Z2 = styled.div`
+    padding-left: 67%;
+`;
+
+export const Z3 = styled.div`
+    padding-left: 70%;
+`;
+
+export const Z4 = styled.div`
+    padding-left: 57%;
+`;
+
+
+
+
+export const NavContent = styled.div`
+    display: none;
+    position: relative;
+    background-color: #0b4a54;
+    min-width: 100%;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 5;
+    color: #fff;
+    border-bottom: 2px solid #000;
+
+
+    ${NavDropdown}:hover & {
+        display: block;
+    }
+`;
+
+
+export const NavContent2 = styled.div`
+    display: none;
+    position: relative;
+    background-color: #0b4a54;
+    min-width: 100%;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 5;
+    color: #fff;
+    border-bottom: 2px solid #000;
+
+
+    ${NavDropdown}:hover & {
+        display: block;
+    }
+`;
+
+
+export const NavContent3 = styled.div`
+    display: none;
+    position: relative;
+    background-color: #0b4a54;
+    min-width: 100%;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    color: #fff;
+    border-bottom: 2px solid #000;
+
+
+    ${NavDropdown}:hover & {
+        display: block;
+    }
+`;
+
+
+export const NavContent4 = styled.div`
+    display: none;
+    position: relative;
+    background-color: #0b4a54;
+    min-width: 100%;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    color: #fff;
+    border-bottom: 2px solid #000;
+
+
+    ${NavDropdown}:hover & {
+        display: block;
+    }
+`;
+
+
+export const NavContent5 = styled.div`
+    display: none;
+    position: relative;
+    background-color: #0b4a54;
+    min-width: 100%;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    color: #fff;
+    border-bottom: 2px solid #000;
+
+
+    ${NavDropdown}:hover & {
+        display: block;
+    }
+`;
+
+
+export const NCLink = styled(Link)`
+    float: none;
+    color: #fff;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+    font-size: 24px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    // border-bottom: 2px solid #000;
+    padding-left: 50px;
+
+    &:hover{
+        background-color: #ddd;
+        color: #000;
+    }
+`;
+
+
+export const ItemsNav = styled.div`
+    height: 50px;
+    display: flex;
+    align-items: center;
+    // background-color: rgba(172, 117, 0, 1);
+    background-color: rgb(108 56 14);
+    z-index: 10;
+
+
+
+    @media (max-width: 648px) {
+        display: none;
+    }
+
+
+
+`;
+
+
+
+export const ItemsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+    padding: 0 24px;
+    max-width: 100%;
+`;
+
+
+
+
+export const Items = styled.ul`
+    display: flex;
+    list-style: none;
+    text-align: center;
+`;
+
+
+
+
+export const ItemsList = styled.li`
+    display: flex;
+    list-style: none;
+    text-align: center;
+    justify-content: center;
+`;
+
+
+
+
+export const Item = styled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1.7rem;
+    height: 100%;
+    cursor: pointer;
+`;
+
+
+
+
+export const IDropdown = styled.div`
+    float: none;
+    overflow: hidden;
+    width: 100%;
+`;
+
+
+
+
+export const ItemIcon = styled(Link)`       
+    color: #fff;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    text-decoration: none;
+    background-color: inherit;
+    font-family: inherit;
+    padding: 0 1.7rem;
+`;
+
+
+
+
+export const ItemContent = styled.div`
+    display: none;
+    position: absolute;
+    min-width: 160px;
+    top: 150px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1000;
+    // background-color: #0b4a54;
+    background-color: #fff;
+    margin-left: 30px;
+
+
+    ${IDropdown}:hover & {
+        display: block;
+    }
+`;
+
+
+
+
+export const ItemLink = styled(Link)`
+    float: none;
+    color: #fff;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+
+
+    &:hover{
+        background-color: #ddd;
+        color: #000;
+    }
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
